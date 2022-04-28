600479ad8c85998381df2868fe3da4ee78c12798
Satyam Lakhera <75569926+satyam0804@users.noreply.github.com>
2022-04-28 13:44:01 +0530

    71A - Way Too Long Words
    tags- strings  *800

---
+++ 71A- WayTooMuchLong.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    string s;
+
+    if (n == 0)
+    {
+        return 0;
+    }
+
+    while (n--)
+    {
+        cin >> s;
+
+        if (s.length() > 10)
+        {
+            cout << s[0] << s.length() - 2 << s[s.length() - 1] << endl;
+        }
+        else
+        {
+            cout << s << endl;
+        }
+    }
+    return 0;
+}
